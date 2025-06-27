f604ace99f7f1b24bfb3aede9f07ec4d923b07a4
emangalloway <emanuelgalloway25@gmail.com>
2025-06-27 01:14:07 -0400

    Tweaking profile controller

--- capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
@@ -3,16 +3,21 @@ package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
 import org.yearup.models.Profile;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/profile")
 @CrossOrigin
 public class ProfileController {
 private ProfileDao profileDao;
+private UserDao userDao;
 
     @Autowired
     public ProfileController(ProfileDao profileDao) {
@@ -20,14 +25,19 @@ private ProfileDao profileDao;
     }
 
     @GetMapping("")
-    public Profile getProfile(int userId){
+    public Profile getProfile(Principal principal){
+        String userName = principal.getName();
+        int userId = userDao.getByUserName(userName).getId();
         return profileDao.getByUserId(userId);
     }
 
-    @PutMapping("{userId}")
-    public Profile addProfile(@PathVariable int userId, @RequestBody Profile profile){
+    @PutMapping("")
+    public Profile updateProfile(@RequestBody Profile profile, Principal principal){
         try {
-             return profileDao.create(profile);
+            String userName = principal.getName();
+            int userId = userDao.getIdByUsername(userName);
+            profile.setUserId(userId);//Make sure userId matched logged in person
+             return profileDao.update(userId,profile);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Oops.. our bad");
         }
@@ -36,7 +46,7 @@ private ProfileDao profileDao;
 
     @PostMapping("{id}")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public Profile updateProfile(@PathVariable int id, @RequestBody Profile profile){
+    public Profile createProfile(@PathVariable int id, @RequestBody Profile profile){
         try {
             return profileDao.update(id,profile);
         } catch (Exception e) {
